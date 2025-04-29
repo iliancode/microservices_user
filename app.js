35eca25b37e88f5998d0208a7a4ec10add0d2798
YanisHlali <yanis.hlali@gmail.com>
2025-04-29 11:59:10 +0200

    fix: configure CORS options for enhanced security and flexibility

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  
+  app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions));
+  
 
 app.use(express.json());
 app.use('/users', userRoutes);
