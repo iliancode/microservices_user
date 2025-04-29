b9786c6f03d43696ab9dcf73c76a22a7d2badea9
l3miage-benaisil <benaissailian@outlook.fr>
2025-04-29 10:11:55 +0200

    corsé

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({ path: '.env.local' });
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 
 dotenv.config();
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 app.use('/users', userRoutes);
