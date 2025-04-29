ab2f244458d2bd98d07503ca3e8074a450b15960
l3miage-benaisil <benaissailian@outlook.fr>
2025-04-29 10:08:21 +0200

    corsé

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 const app = express();
-app.use(express.json());
+app.use(cors())
 
+app.use(express.json());
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
