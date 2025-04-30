0ade11d16c96c20bc6874b56c4281e8aaefd0bf9
YanisHlali <yanis.hlali@gmail.com>
2025-04-30 15:07:09 +0200

    Remove duplicate routes for menu and orders in userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,12 +8,14 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.get('/menu', getAllMenus);
-
 router.get('/orders', protect, getUserOrders);
 router.delete('/:id', protect, authorize('admin'), deleteUser);
 router.get('/all', listRestaurants);
 router.get('/allLivreurs', protect, listLivreurs);
 // Route pour récupérer un restaurant par son ID
 router.get('/:id', getRestaurantById);
+
+router.get('/menu', getAllMenus);
+router.get('/orders', protect, getUserOrders);
 
 module.exports = router;
